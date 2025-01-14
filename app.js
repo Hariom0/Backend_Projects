04681cd62fec874d35ed9770f4797e1d7cd67045
Hariom0 <hariomsing7323@gmail.com>
2025-01-14 11:50:00 +0530

    change some business logic

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 6969
 app.use(express.json())
 
 let bookId = 0;
-let arr = [
+let allBook = [
     {
         "title": "The Man Who Knew Infinity",
         "author": "Salman Rushdie",
@@ -45,7 +45,7 @@ app.get("/books", (req, res) => {
 
 
     // Filter the array based on the query parameters If True then book will be included and If false then don't
-    let filteredBooks = arr.filter(book => {
+    let filteredBooks = allBook.filter(book => {
         return (!author || book.author.toLowerCase() === author.toLowerCase()) &&
         (!publish || book.publish.toString() === publish) &&
         (!genre || book.genre.toLowerCase() === genre.toLowerCase());
@@ -54,7 +54,7 @@ app.get("/books", (req, res) => {
 
     //  return the full array, If No query is provided
     if (!author && !publish && !genre) {
-        return res.send(arr);
+        return res.send(allBook);
     }
 
     // If No filter match, Then Retrun empty array
@@ -71,27 +71,23 @@ app.get("/books", (req, res) => {
 app.post("/books", (req, res) => {
     let book = req.body
     book.id = bookId++;
-    arr.push(book)
+    allBook.push(book)
     res.status(200).send(`Book ${book.title} by ${book.author} is added Sucessfully...`)
 })
 app.get("/books/:id", (req, res) => {
-    let book = arr.find(e => e.id === Number(req.params.id))
+    let book = allBook.find(e => e.id === Number(req.params.id))
     if (!book) res.status(404).send(`Book of id : ${req.params.id} Not Found!`)
     res.status(200).send(book)
 })
-// app.get("/books/?genre", (req, res) => {
-//     let book = arr.find(e => e.genre == (req.params.genre))
-//     console.log(req.params.genre)
-//     res.status(200).send(book)
-// })
+
 
 app.delete("/books/:id", (req, res) => {
 
-    let book = arr.find(e => e.id === Number(req.params.id))
+    let book = allBook.find(e => e.id === Number(req.params.id))
     if (!book) {
         res.status(404).send(`Book of id : ${req.params.id} Not Found!`)
     } else {
-        arr = arr.filter(e => e !== book)
+        allBook = allBook.filter(e => e !== book)
         res.status(200).send(`Book "${(book.title)}" with id : ${req.params.id} is removed sucessfully`)
     }
 
